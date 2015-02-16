884ef68feeb69ddab5a78a0e5d7ae8a42aaef708
llllllllll <joe@quantopian.com>
2015-02-16 16:26:49 -0500

    MAINT: Removes old test file and cleans up imports in __init__.py

--- metautils/__init__.py
+++ metautils/__init__.py
@@ -12,7 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from metautils.template import T, templated, compose
+from metautils.template import T, templated
+from metautils.compat import compose
 
 __all__ = [
     'T',
