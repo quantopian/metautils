1c212b196cbdb3442564091f120e0770f1324630
Joe Jevnik <joe@quantopian.com>
2015-03-31 15:18:41 -0400

    DEV: Export singleton

--- metautils/__init__.py
+++ metautils/__init__.py
@@ -12,11 +12,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from metautils.template import T, templated
 from metautils.compat import compose
+from metautils.template import T, templated
+from metautils.singleton import Singleton
 
 __all__ = [
     'T',
+    'Singleton',
     'templated',
     'compose',
 ]
